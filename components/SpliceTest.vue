d3c31f08e4be166f85809174f4fe10986064c143
Jamie Naioti <naioti@gmail.com>
2024-02-12 12:30:54 -0600

    example of GSAP controlling Spline 3D object, the layer name in Spline file is 'pugHead' that is accessible on mounted

--- components/SpliceTest.vue
+++ components/SpliceTest.vue
@@ -17,12 +17,19 @@
         },
     });
 
+    import gsap from 'gsap'
+    // import scrollTrigger from 'gsap/dist/ScrollTrigger'
+
     onMounted(async () => {
         const app = new Application(spliceCanvas.value);
         await app.load(state.spline.scene)
+        const pugHead = app.findObjectByName('PugHead');
+        console.log('pugHead', pugHead);
         state.spline.app = app;
         state.spline.isLoaded = true;
         
+        // const mainTL = gsap.timeline({repeat: 1111, yoyo:true});
+        // mainTL.fromTo(pugHead.scale, {x: 0.5, y: 0.5, z: 0.5}, {x: 1, y: 1, z: 1, duration: 1})
     })
 </script>
 <style scoped lang="scss">
