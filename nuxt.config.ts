7c67efeb88a0eff8c9d4591dbd169c5084896305
Jamie Naioti <naioti@gmail.com>
2024-02-23 10:56:35 -0600

    upgrade Nuxt to 3.10.3, removes unused node-modules, specifies static site build 'ssr: false' in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: false,
     modules: [
         '@nuxtjs/fontaine'
     ],
@@ -20,5 +21,4 @@ export default defineNuxtConfig({
         },
     },
     spaLoadingTemplate: false,
-    
 })
